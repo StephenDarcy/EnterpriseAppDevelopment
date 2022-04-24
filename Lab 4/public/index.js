044dc8fb4cb38e2959bc7d38655cb1840d3c7a3d
Stephen Darcy <stephendarcy123@gmail.com>
2022-04-24 15:30:19 +0100

    more work

--- Lab 4/public/index.js
+++ Lab 4/public/index.js
@@ -4,18 +4,75 @@ document.getElementById("getAll").onclick = function () {
 document.getElementById("getOne").onclick = function () {
   getOne();
 };
+document.getElementById("insertOne").onclick = function () {
+  insertOne();
+};
 let getAllTable = document.getElementById("getAllTable");
 let oneBox = document.getElementById("oneBox");
+let insertBox = document.getElementById("insertBox");
 
 oneBox.style.display = "none";
+insertBox.style.display = "none";
+
+function insertOne() {
+  oneBox.style.display = "none";
+  getAllTable.style.display = "none";
+  insertBox.style.display = "";
+}
 
 function getOne() {
   getAllTable.style.display = "none";
   oneBox.style.display = "";
+  insertBox.style.display = "none";
+  var currentColour;
+  let inputValue = document.getElementById("colourid").value;
+
+  if (inputValue) {
+    currentColour = inputValue;
+  } else {
+    currentColour = 0;
+  }
+
+  let request = new XMLHttpRequest();
+  request.open("GET", `/colours/${currentColour}`);
+  request.send();
+  request.onload = () => {
+    if (request.status === 200) {
+      let data = JSON.parse(request.response);
+      populateOne(data);
+    } else {
+      console.log("Error fetching all colours");
+    }
+  };
+}
+
+function populateOne(data) {
+  let id = document.getElementById("colour-id");
+  let hex = document.getElementById("colour-hex");
+  let rgb = document.getElementById("colour-rgb");
+  let hsl = document.getElementById("colour-hsl");
+  let name = document.getElementById("colour-name");
+  let box = document.getElementById("colour-display");
+
+  box.style.backgroundColor = data.hexString;
+  id.innerHTML = data.colorId;
+  hex.innerHTML = data.hexString;
+  rgb.innerHTML =
+    "rgb(" + data.rgb.r + ", " + data.rgb.g + ", " + data.rgb.b + ")";
+  hsl.innerHTML =
+    "hsl(" +
+    data.hsl.h.toFixed() +
+    ", " +
+    data.hsl.s +
+    "%, " +
+    data.hsl.l +
+    "%)";
+  name.innerHTML = data.name;
 }
 
 function getAll() {
   getAllTable.style.display = "";
+  insertBox.style.display = "none";
   oneBox.style.display = "none";
   let request = new XMLHttpRequest();
   request.open("GET", "/colours");
@@ -67,7 +124,7 @@ function generateTable(data) {
     .join("");
 
   return `
-	<table class="table table-dark">
+	<table class="table table-dark ">
 		<thead>
 			<tr>${headerRow}</tr>
 		<thead>
